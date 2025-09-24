b2fec1706c9c2d9662435f6467a43ec1857541dc
Nadra Ibrahim <nadra.ibrahim@nashtechglobal.com>
2025-09-24 14:43:34 +0530

    added secrets in config file

---
+++ my-react-app/src/config.js
@@ -0,0 +1,4 @@
+const AWS_SECRET_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
+const DB_PASSWORD = "SuperSecurePassword123!";
+const GITHUB_TOKEN = "ghp_1234567890EXAMPLETOKEN";
+const SECRET_KEY = "mySuperSecretKey123!";
